b1f09c2b0ad487e32c88c1aaf9216e3546766df2
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-01-22 22:30:59 +0000

    add validations for fields to ensure they have data entered to post a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,17 @@
 class Message < ActiveRecord::Base
 
+  validates :title, 
+    presence: true,
+    length: { maximum: 50 }
+
+  validates :content,
+    presence: true,
+    length: { maximum: 200 }
+
+  validates :author,
+    presence: true,
+    length: { maximum: 30 }
+
+
+
 end
