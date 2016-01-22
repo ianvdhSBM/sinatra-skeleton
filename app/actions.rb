d1bffa08e076e6916f5eb35f1f1a0742ec30fe51
Ian Van Den Heuvel <ianvdh@gmail.com>
2016-01-22 21:55:30 +0000

    update .erb files to make posts on website using Sinatra

---
+++ app/actions.rb
@@ -0,0 +1,29 @@
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end 
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  binding.pry
+  @message.save
+  redirect '/messages'
+end
